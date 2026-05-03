7bab4e2f7a7eb5d40ed1dee35252c859d587bdd5
doodledoodle128-prog <doodle.doodle128@gmail.com>
2026-05-03 16:38:09 -0400

    Add files via upload

---
+++ box.py
@@ -0,0 +1,19 @@
+import turtle
+
+sc = turtle.Screen()
+sc.bgcolor("Pink")
+sq = turtle.Turtle()
+
+sq.forward(100)
+sq.right(90)
+
+sq.forward(100)
+sq.right(90)
+
+sq.forward(100)
+sq.right(90)
+
+sq.forward(100)
+sq.right(90)
+
+turtle.done()
